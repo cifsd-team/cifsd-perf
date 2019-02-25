6eed0d260472d297459aa741e8e702a391230532
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2019-02-25 10:45:03 +0900

    add tracing-on-off script
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

---
+++ tracing-on-off.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /sys/kernel/debug/tracing/
+
+if [ "z$1" == "zon" ]; then
+	echo ':mod:cifsd' > set_ftrace_filter
+	echo 'cifsd*:mod:cifsd' >> set_ftrace_filter
+	echo 'smb*:mod:cifsd' >> set_ftrace_filter
+	echo function_graph > current_tracer
+	echo 1 > tracing_on
+else
+	echo 0 > tracing_on
+	cat trace >> /tmp/__trace__
+fi
